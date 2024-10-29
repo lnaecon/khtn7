04b9f3b86415aaa0731d54b8edd661b7f3331069
lnaecon <64239380+lnaecon@users.noreply.github.com>
2024-10-29 20:33:47 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -366,7 +366,7 @@ const EnglishReview = () => {
                 <div className="aspect-video">
                   <iframe 
                     className="w-full h-full rounded-lg"
-                    src="https://www.youtube.com/embed/LhveTGblGHY?si=VIy-XGlQWtNTiMQJ" 
+                    src="https://www.youtube.com/embed/LhveTGblGHY" 
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
